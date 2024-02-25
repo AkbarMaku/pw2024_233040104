fa15536c756ab4a01e3cbbef423a3288fdacf8ae
AkbarMaku <aakbarmaku@gmail.com>
2024-02-25 08:34:31 +0700

    first commit

---
+++ kuliah/pertemuan3/latihan1.php
@@ -0,0 +1,19 @@
+<?php
+echo "Hello World!";
+echo "<br>";
+echo "Kuliah Pemoograman Web";
+echo "<br>";
+echo 123;
+echo "<br>";
+echo 3.14;
+echo "<br>";
+echo true;
+echo false;
+echo "<br>";
+echo 'Akbar';
+echo "<br>";
+echo "Jum'at";
+echo "<br>";
+echo 'Nama saya "Akbar"';
+echo "<br>";
+echo '"Akbar" kuliah di hari ' . "Jum'at";
---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,5 @@
+<?php
+$name = "Akbar";
+$age = 18;
+$university_name = "Universitas Pasundan";
+echo "<p>Halo, Nama saya $name saya $age tahun, kuliah di $university_name</p>";
